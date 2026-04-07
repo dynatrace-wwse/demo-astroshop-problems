bae2fa2eb0d04f86b8774e86c76c84e0b17953f3
Sergio Hinojosa <hj.sergio@gmail.com>
2026-04-07 12:38:47 +0000

    fix integration test
    Signed-off-by: Sergio Hinojosa <hj.sergio@gmail.com>

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -5,12 +5,3 @@ source .devcontainer/util/source_framework.sh
 printInfoSection "Running integration Tests for $RepositoryName"
 
 #TODO: In here you add your assertions
-#assertRunningPod dynatrace operator
-
-#assertRunningPod dynatrace activegate
-
-#assertRunningPod dynatrace oneagent
-
-assertRunningPod todoapp todoapp
-
-assertRunningApp 30100
